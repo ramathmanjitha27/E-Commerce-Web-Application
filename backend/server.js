08c8176da386167ce09535bea9648d2cf0ef0121
Ramath <ramathmanjitha@gmail.com>
2022-07-12 11:31:11 +0530

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,21 @@ const mongoose = require('mongoose')
 
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv');
 
 require('dotenv').config();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
+
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL);
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('MongoDB connection success')
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
